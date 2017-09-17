e060e9d89a8961519d4dfd682ac6d131b4728b88
Chad Kunde <Kunde21@gmail.com>
2017-09-16 17:23:10 -0700

    asm/f64: Added stubs, tests, and noasm for gemv functions.

--- internal/asm/f64/ge_amd64.go
+++ internal/asm/f64/ge_amd64.go
@@ -13,3 +13,23 @@ func Ger(m, n uintptr, alpha float64,
 	x []float64, incX uintptr,
 	y []float64, incY uintptr,
 	a []float64, lda uintptr)
+
+// GemvN computes
+//  y = alpha * a * x + beta * y
+// where A is an m×n dense matrix, x and y are vectors, and alpha and beta are scalars.
+func GemvN(m, n uintptr,
+	alpha float64,
+	a []float64, lda uintptr,
+	x []float64, incX uintptr,
+	beta float64,
+	y []float64, incY uintptr)
+
+// GemvT computes
+//  y = alpha * A^T * x + beta * y
+// where A is an m×n dense matrix, x and y are vectors, and alpha and beta are scalars.
+func GemvT(m, n uintptr,
+	alpha float64,
+	a []float64, lda uintptr,
+	x []float64, incX uintptr,
+	beta float64,
+	y []float64, incY uintptr)
